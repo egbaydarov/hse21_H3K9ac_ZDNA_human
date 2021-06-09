79c1588556a806627cfe768894d01c793b4b756e
egbaydarov <you@example.com>
2021-06-10 00:07:50 +0300

    change chIdsecker

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -1,14 +1,24 @@
 
 source('lib.R')
 
+###
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+# BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
+
 library(ChIPseeker)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 library(clusterProfiler)
 
 ###
 
-NAME <- 'H3K9ac_H1.ENCFF754YLR.hg19.filtered'
+#NAME <- 'H3K9ac_H1.intersect_with_DeepZ'
+#NAME <- 'DeepZ'
 #NAME <- 'H3K9ac_H1.ENCFF584EB0.hg19.filtered'
+NAME <- 'H3K9ac_H1.ENCFF754YLR.hg19.filtered'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 ###
